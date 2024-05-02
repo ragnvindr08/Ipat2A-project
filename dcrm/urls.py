eeb3c3faba6aa1d87c3e39ea5aef349d2256ebf7
ragnvindr08 <johnstevenimperialcaspe08@gmail.com>
2024-05-02 19:23:49 +0800

    initial commit

---
+++ dcrm/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('register', views.register, name='register'),
+    path('logout/', views.logout_user, name='logout'),
+    path('update_record/<int:pk>', views.update_record, name='update_record'),
+    path('add_record/', views.add_record, name='add_record'),
+    path('delete_record/<int:pk>', views.delete_record, name='delete_record'),
+]
